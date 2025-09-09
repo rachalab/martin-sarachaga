7ae6b78dd04bb67633a95fae28a92ce54b25139f
neltin <gautofreelance@gmail.com>
2025-09-09 20:29:40 -0300

    Carpetas y subcarpetas

---
+++ src/app/subasta-presencial/[subastaId]/page.jsx
@@ -0,0 +1,7 @@
+export default function Page({ params }) {
+  return (
+    <>
+      <h1>Subasta-presencial/{params.subastaId}</h1>
+    </>
+  );
+}
---
+++ src/app/subasta-presencial/page.jsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <>
+      <h1>Subasta-presencial</h1>
+    </>
+  );
+}
