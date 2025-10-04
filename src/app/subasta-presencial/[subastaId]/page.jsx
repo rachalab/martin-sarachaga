41059964e46e72c9c5343857732c15e27d88d1db
pentflacso <cmilillo@flacso.org.ar>
2025-10-03 21:48:51 -0600

    Cambios Ari y Martín

--- src/app/subasta-presencial/[subastaId]/page.jsx
+++ src/app/subasta-presencial/[subastaId]/page.jsx
@@ -4,11 +4,10 @@ import AuctionPrefilter from "../../../components/builder/AuctionPrefilter/Aucti
 import Footer from "../../../components/structure/Footer/Footer";
 import apiGetServer from "@/lib/apiGetServer";
 import { notFound } from "next/navigation";
-import styles from "./page.module.scss"
+import styles from "./page.module.scss";
 
 export async function generateMetadata({ params }) {
-
-  const{ subastaId } = await params;
+  const { subastaId } = await params;
 
   const data = await apiGetServer({
     url: `category/${subastaId}`,
@@ -19,38 +18,38 @@ export async function generateMetadata({ params }) {
 
   return {
     title: `Subasta Nro ${data?.subasta?.nro}`,
-    description: data?.subasta?.description,  
+    description: data?.subasta?.description,
   };
 }
 
 export default async function Page({ params }) {
-
   const { subastaId } = await params;
 
   const data = await apiGetServer({
-    url: `category.php?id=${subastaId}`,
+    url: `category/${subastaId}`,
   });
 
   //Si no hay datos redireccionamos
   if (!data?.categorias) return notFound();
 
-  
   return (
-    <MainWrapper> 
-      <Heading data={{heading: 'SUBASTA PRESENCIAL'}} />
-        <div className={styles.nights}>
-          {data?.noches.map((data, i) => {               
-                return (
-                  <div className={styles.night} key={i}>
-                    <p className={styles.red}>Noche {data.noche}</p>
-                    <p>{data.dia.format.substring(data.dia.format.indexOf(',') + 1)}</p> 
-                    <p>{data.horario.format} H.</p>
-                  </div>
-                );
-            })}
-        </div>
-        <AuctionPrefilter subastaId={subastaId} links={data?.categorias} />
-        <Footer />
-    </MainWrapper> 
+    <MainWrapper>
+      <Heading data={{ heading: "SUBASTA PRESENCIAL" }} />
+      <div className={styles.nights}>
+        {data?.noches.map((data, i) => {
+          return (
+            <div className={styles.night} key={i}>
+              <p className={styles.red}>Noche {data.noche}</p>
+              <p>
+                {data.dia.format.substring(data.dia.format.indexOf(",") + 1)}
+              </p>
+              <p>{data.horario.format} H.</p>
+            </div>
+          );
+        })}
+      </div>
+      <AuctionPrefilter subastaId={subastaId} links={data?.categorias} />
+      <Footer />
+    </MainWrapper>
   );
 }
--- src/app/venta-privada/page.jsx
+++ src/app/venta-privada/page.jsx
@@ -6,7 +6,6 @@ import apiGetServer from "@/lib/apiGetServer";
 import { notFound } from "next/navigation";
 
 export async function generateMetadata() {
-
   const data = await apiGetServer({
     url: `direct-sale`,
   });
@@ -15,13 +14,12 @@ export async function generateMetadata() {
   if (!data?.categorias) return notFound();
 
   return {
-    title: `Ventas online`,
-    description: `Ventas online`,  
+    title: `Venta privada`,
+    description: `Ventas privada`,
   };
 }
 
 export default async function Page() {
-
   const data = await apiGetServer({
     url: `direct-sale`,
   });
@@ -29,19 +27,17 @@ export default async function Page() {
   //Si no hay datos redireccionamos
   if (!data?.categorias) return notFound();
 
-  
   return (
-    <MainWrapper> 
-      <Heading data={{heading: 'Ventas online'}} />
-      
+    <MainWrapper>
+      <Heading data={{ heading: "Venta privada" }} />
+
       <pre>
         {data?.categorias
           ? JSON.stringify(data.categorias, null, 2)
           : "No hay categorías"}
       </pre>
 
-
-        <Footer />
-    </MainWrapper> 
+      <Footer />
+    </MainWrapper>
   );
 }
