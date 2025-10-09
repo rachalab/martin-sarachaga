ad02adf92fb39302c17d569945edcbde2f2b6846
neltin <gautofreelance@gmail.com>
2025-10-09 18:36:32 -0300

    Incorporacion de header y footer a paginas de error

--- src/app/error.js
+++ src/app/error.js
@@ -1,12 +1,17 @@
 'use client';
 
 import Link from 'next/link';
+import MainWrapper from "../components/structure/MainWrapper/MainWrapper";
+import HeaderNav from "../components/structure/HeaderNav/HeaderNav";
+import Footer from "../components/structure/Footer/Footer";
 
 export default function Error() {
   return (
-    <div>
+    <MainWrapper>
+      <HeaderNav />
       <p>Lo sentimos, ocurrió un error.</p>
       <Link href="/">Volver a la página de inicio</Link>
-    </div>
+      <Footer />
+    </MainWrapper>
   );
 }
--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,11 +1,16 @@
 import Link from 'next/link';
+import MainWrapper from "../components/structure/MainWrapper/MainWrapper";
+import HeaderNav from "../components/structure/HeaderNav/HeaderNav";
+import Footer from "../components/structure/Footer/Footer";
 
 export default function NotFound() {
   return (
-    <div>
+    <MainWrapper>
+      <HeaderNav />
       <h2>Página no encontrada</h2>
       <p>Lo sentimos, no pudimos encontrar el recurso que estás buscando.</p>
       <Link href="/">Volver al inicio</Link>
-    </div>
+      <Footer />      
+    </MainWrapper>
   );
 }
