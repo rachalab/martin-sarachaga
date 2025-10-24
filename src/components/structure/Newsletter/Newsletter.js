d380a77420c93623b99b65664060ecffbeb8cc9c
neltin <gautofreelance@gmail.com>
2025-10-24 14:06:14 -0300

    LocalStorage newsLetter

--- src/components/structure/Newsletter/Newsletter.js
+++ src/components/structure/Newsletter/Newsletter.js
@@ -9,11 +9,20 @@ export default function Newsletter() {
   const [isClosing, setIsClosing] = useState(false);
   const [newsletterStatus, setNewsletterStatus] = useState(false);
 
+  const [validate, setValidate] = useState(false);
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+
   useEffect(() => {
+    const subscribed = localStorage.getItem("newsletterSubscribed");
+
     setIsBrowser(true);
-    setTimeout(() => {    
-      setNewsletterStatus(true);  
-    }, 2000);
+
+    if (!subscribed) {
+      setTimeout(() => {    
+        setNewsletterStatus(true);  
+      }, 2000);
+    }
   }, []);
 
   function closeFilters() {
@@ -23,6 +32,29 @@ export default function Newsletter() {
     }, 800);
   }
 
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const name = e.target.user_name.value.trim();
+    const email = e.target.user_mail.value.trim();
+
+    if (!name || !emailRegex.test(email)) {
+      setValidate(true);
+      return;
+    }
+
+    //Fetch API
+
+    // Guardar en localStorage que ya se suscribió
+    localStorage.setItem("newsletterSubscribed", "true");
+
+    // Cerrar el modal con animación
+    closeFilters();
+  }
+
+
+
+
   const newletterContent = (
     <div className={!isClosing ? `${styles.wrapper}` : `${styles.wrapper} ${styles.closing}`}>
       <div className={styles.container}>
@@ -30,12 +62,15 @@ export default function Newsletter() {
         <button onClick={closeFilters} className={styles.close_btn} />        
         <h4 className={styles.title}>Enterate primero</h4>
         <p>No te pierdas las próximas grandes subastas y las pequeñas subastas online.</p>
-        <form className={styles.form}>
+
+        { validate && <p className={styles.validate_msg}>Uno o más campos son incorrectos.</p> }
+
+        <form className={styles.form} onSubmit={handleSubmit}>
           <label className={styles.label}>Nombre Apellido</label>
           <input type="text" id="name" name="user_name" className={styles.input}/>         
           <label className={styles.label}>E-mail</label>
           <input type="email" id="mail" name="user_mail" className={styles.input}/>
-          <button type="submit" className={styles.submit_btn}>Registrarme</button>                 
+          <button type="submit" className={styles.submit_btn}>Registrarme</button>
         </form>
  
       </div>
